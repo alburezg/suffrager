97ae4b69857a5b657f113858e7f80b48d7a31fba
alburezg <gatemonte@gmail.com>
2019-04-13 16:05:46 -0500

    changed name to suffrager

--- R/suffrage.R
+++ R/suffrage.R
@@ -1,5 +1,5 @@
-#' @title suffrage
-#' @name suffrage
+#' @title suffrager
+#' @name suffrager
 #' @docType package
 #' @details list of palettes from suffragette and suffragist materials
 #' @description list of palettes from suffragette and suffragist materials
